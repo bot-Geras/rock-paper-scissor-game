c5686394bc672e2dc28c80ae5b05bda2bbd71155
Brian <briandave771@gmail.com>
2022-12-03 15:29:33 +0300

    Refactore my reload button

--- index.js
+++ index.js
@@ -5,29 +5,27 @@ const possibleChoice = document.querySelectorAll("button");
 const resetBtn = document.getElementById("Reset");
 const playerScoreHtml = document.getElementById("player");
 const computerScoreHtml = document.getElementById("computer");
-const movesLeft = document.getElementById("moves")
-const displayContainer = document.getElementById("container")
+const movesLeft = document.getElementById("moves");
+const displayContainer = document.getElementById("container");
 
 let userChoice;
 let computerChoice;
 let results;
 
-let playerScore = 0
-let computerScore = 0
-let moves = 0
+let playerScore = 0;
+let computerScore = 0;
+let moves = 0;
 
 function getUserChoice(e) {
   userChoice = e.target.id;
   userDisplay.innerHTML = userChoice;
-  moves++
-  movesLeft.textContent = `${10 - moves}`
+  moves++;
+  movesLeft.textContent = `${10 - moves}`;
   generateComputerChoice();
   getResults();
-  if(moves === 10){
-    
-    reloadGame()
+  if (moves === 10) {
+    reloadGame();
   }
-  
 }
 
 function generateComputerChoice() {
@@ -47,44 +45,40 @@ function generateComputerChoice() {
 }
 
 function getResults() {
-  
   if (computerChoice === userChoice) {
     results = "its a Draw";
   }
   if (computerChoice === "rock" && userChoice === "paper") {
     results = "You Won";
     playerScore++;
-    playerScoreHtml.textContent = playerScore
+    playerScoreHtml.textContent = playerScore;
   }
   if (computerChoice === "rock" && userChoice === "scissors") {
     results = "You lost";
     computerScore++;
-    computerScoreHtml.textContent = computerScore
+    computerScoreHtml.textContent = computerScore;
   }
   if (computerChoice === "paper" && userChoice === "scissors") {
     results = "You win";
     playerScore++;
-    playerScoreHtml.textContent = playerScore
+    playerScoreHtml.textContent = playerScore;
   }
   if (computerChoice === "paper" && userChoice === "Rock") {
     results = "You lost";
     computerScore++;
-    computerScoreHtml.textContent = computerScore
+    computerScoreHtml.textContent = computerScore;
   }
   if (computerChoice === "scissors" && userChoice === "rock") {
     results = "You Won";
     playerScore++;
-    playerScoreHtml.textContent = playerScore
+    playerScoreHtml.textContent = playerScore;
   }
   if (computerChoice === "scissors" && userChoice === "paper") {
     results = "you lost";
     computerScore++;
-    computerScoreHtml.textContent = computerScore
+    computerScoreHtml.textContent = computerScore;
   }
   resultDisplay.innerHTML = results;
- 
-  
- 
 }
 function gameOver(playerScore, computerScore) {
   playerScoreHtml.innerHTML = playerScore;
@@ -94,11 +88,7 @@ function gameOver(playerScore, computerScore) {
 possibleChoice.forEach((choice) =>
   choice.addEventListener("click", getUserChoice)
 );
-function reloadGame () {
-  
-  computerDisplay.innerHTML = "";
-  userDisplay.innerHTML = "";
-  resultDisplay.innerHTML = "";
- 
+function reloadGame() {
+  window.location.reload();
 }
 resetBtn.addEventListener("click", reloadGame);
