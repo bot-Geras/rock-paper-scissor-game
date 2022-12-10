ba647de5d06803c16243bc3b43935ea08ae3fc8b
Brian <briandave771@gmail.com>
2022-12-10 15:57:00 +0300

    Indent the code

--- index.js
+++ index.js
@@ -2,58 +2,54 @@ const computerDisplay = document.getElementById("computer-results");
 const userDisplay = document.getElementById("user-results");
 const resultDisplay = document.getElementById("result");
 const possibleChoice = document.querySelectorAll("button");
-
+const player = document.querySelector("#player");
+const computer = document.querySelector("#computer");
 let userChoice;
 let computerChoice;
-let results;
-
-function generateComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
-
-  if (randomNumber === 1) {
-    computerChoice = "rock";
-  }
-  if (randomNumber === 2) {
-    computerChoice = "scissors";
-  }
-  if (randomNumber === 3) {
-    computerChoice = "paper";
-  }
-
-  computerDisplay.innerHTML = computerChoice;
-}
-
-function getResults() {
-  if (computerChoice === userChoice) {
-    results = "its a Draw";
-  }
-  if (computerChoice === "rock" && userChoice === "paper") {
-    results = "You Won";
-  }
-  if (computerChoice === "rock" && userChoice === "scissors") {
-    results = "You lost";
-  }
-  if (computerChoice === "paper" && userChoice === "scissors") {
-    results = "You win";
-  }
-  if (computerChoice === "paper" && userChoice === "Rock") {
-    results = "You lost";
-  }
-  if (computerChoice === "scissors" && userChoice === "rock") {
-    results = "You Won";
-  }
-  if (computerChoice === "scissors" && userChoice === "paper") {
-    results = "you lost";
-  }
-  resultDisplay.innerHTML = results;
-  console.log(results);
-}
+let score = 0;
 
 possibleChoice.forEach((choice) =>
   choice.addEventListener("click", (e) => {
     userChoice = e.target.id;
-    userDisplay.innerHTML = userChoice;
+    userDisplay.textContent = userChoice;
     generateComputerChoice();
     getResults();
   })
 );
+
+function generateComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * possibleChoice.length);
+
+  if (randomNumber === 0) {
+    computerChoice = "rock";
+  }
+  if (randomNumber === 1) {
+    computerChoice = "scissors";
+  }
+  if (randomNumber === 2) {
+    computerChoice = "paper";
+  }
+  computerDisplay.textContent = computerChoice;
+}
+function getResults() {
+  switch (computerChoice + userChoice) {
+    case "scissorspaper":
+    case "rockscissors":
+    case "paperrock":
+      resultDisplay.textContent = "You Lost";
+      computer.textContent = ++score;
+      break;
+    case "paperscissors":
+    case "scissorsrock":
+    case "rockpaper":
+      resultDisplay.textContent = "You Win";
+      player.textContent = ++score;
+      break;
+    case "paperpaper":
+    case "scissorsscissors":
+    case "rockrock":
+      resultDisplay.textContent = "Its a draw";
+      break;
+  }
+}
+
